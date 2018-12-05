51ef52203190e8b47ddc39e007a7a51c7d72b26a
Makentoshe <mkliverout@gmail.com>
2018-12-05 19:42:25 +0300

    [refactor] Change view holder's views types from EditText to TextView

--- app/src/main/java/com/makentoshe/uselessactions/ViewHolder.kt
+++ app/src/main/java/com/makentoshe/uselessactions/ViewHolder.kt
@@ -1,6 +1,5 @@
 package com.makentoshe.uselessactions
 
-import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,17 +8,17 @@ import androidx.appcompat.app.AppCompatActivity
  */
 class ViewHolder(activity: AppCompatActivity) {
     val passportFirstNameEditText = activity.findViewById<TextView>(R.id.first_name)
-    val passportLastNameEditText = activity.findViewById<EditText>(R.id.last_name)
-    val birthdayEditText = activity.findViewById<EditText>(R.id.birthday)
-    val loginEditText = activity.findViewById<EditText>(R.id.login)
-    val mailEditText = activity.findViewById<EditText>(R.id.mail)
-    val passwordEditText = activity.findViewById<EditText>(R.id.password1)
-    val repeatPasswordEditText = activity.findViewById<EditText>(R.id.password2)
-    val secretQuestionEditText = activity.findViewById<EditText>(R.id.question)
-    val secretAnswerEditText = activity.findViewById<EditText>(R.id.answer)
-    val phoneNumberEditText = activity.findViewById<EditText>(R.id.phone)
-    val countryEditText = activity.findViewById<EditText>(R.id.country)
-    val cityEditText = activity.findViewById<EditText>(R.id.city)
-    val streetEditText = activity.findViewById<EditText>(R.id.street)
-    val houseEditText = activity.findViewById<EditText>(R.id.house)
+    val passportLastNameEditText = activity.findViewById<TextView>(R.id.last_name)
+    val birthdayEditText = activity.findViewById<TextView>(R.id.birthday)
+    val loginEditText = activity.findViewById<TextView>(R.id.login)
+    val mailEditText = activity.findViewById<TextView>(R.id.mail)
+    val passwordEditText = activity.findViewById<TextView>(R.id.password1)
+    val repeatPasswordEditText = activity.findViewById<TextView>(R.id.password2)
+    val secretQuestionEditText = activity.findViewById<TextView>(R.id.question)
+    val secretAnswerEditText = activity.findViewById<TextView>(R.id.answer)
+    val phoneNumberEditText = activity.findViewById<TextView>(R.id.phone)
+    val countryEditText = activity.findViewById<TextView>(R.id.country)
+    val cityEditText = activity.findViewById<TextView>(R.id.city)
+    val streetEditText = activity.findViewById<TextView>(R.id.street)
+    val houseEditText = activity.findViewById<TextView>(R.id.house)
 }
